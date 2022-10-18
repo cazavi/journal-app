a8b27d689162fe74b2a6c4bf9e6db7bab2aef123
Carolina <carolinazapatavidal@gmail.com>
2022-10-18 16:47:00 -0300

    sidebar and navbar components update

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import {Typography} from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum error omnis suscipit quis quam, libero illum facilis, eveniet pariatur odit consectetur velit assumenda dolorem ab dolor voluptatibus aliquam odio sed.</Typography>
+    </JournalLayout>
   )
 }
