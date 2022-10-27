60b2ff89f14c66f472262781d9efa5e2b0e67b16
Carolina <carolinazapatavidal@gmail.com>
2022-10-27 17:46:47 -0300

    noteview update

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,10 @@
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views/NothingSelectedView';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NothingSelectedView />
+      <NoteView />
     </JournalLayout>
   )
 }
